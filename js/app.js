a01e0081782a996548c4bba76316993ef32a6a6f
Ismail Bourbie <bourbieismail@gmail.com>
2021-08-28 14:01:52 +0100

    added landing page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 $(document).ready(function () {
+
+    // Landing page height
+
+    var windowHeight = $(window).height(),
+        navHeight = $('.navbar').innerHeight();
+
+    $('.landing-page').height(windowHeight - navHeight);
 });
