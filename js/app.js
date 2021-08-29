2c22e9172da742eca0cb01eefc00703f83cd9e50
Ismail Bourbie <bourbieismail@gmail.com>
2021-08-29 14:08:49 +0100

    added WORKS section

--- js/app.js
+++ js/app.js
@@ -6,4 +6,20 @@ $(document).ready(function () {
         navHeight = $('.navbar').innerHeight();
 
     $('.landing-page').height(windowHeight - navHeight);
+
+
+    // shuffle images
+    $('.button-list ul li').click(function () {
+        let dataClass = $(this).data('class');
+        console.log(dataClass);
+        $(this).addClass('active').siblings().removeClass('active');
+
+        if (dataClass == 'all') {
+            $('.shuffle-img > .row > div').css('opacity', '1');
+        } else {
+            $('.shuffle-img > .row > div').css('opacity', '.08');
+            $(dataClass).css('opacity', '1');
+        }
+
+    });
 });
