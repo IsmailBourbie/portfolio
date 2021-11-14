a72abcc459479cbf78be2dc65858b9ca750b5046
Ismail Bourbie <bourbieismail@gmail.com>
2021-11-14 14:06:33 +0100

    added smooth scroll, added animation to the nav on scroll, changed hover effect of nav links

--- js/app.js
+++ js/app.js
@@ -1,12 +1,46 @@
 $(document).ready(function () {
 
+
+
+
+    // on reload page check the scroll
+    if ($(window).scrollTop() >= 470) {
+        $("nav.navbar").addClass("navbar-scrolled");
+    } else {
+        $("nav.navbar").removeClass("navbar-scrolled");
+    }
+    // else if (lastScrolTop > scrollTop) {
+    //     navbarElement.animate({ top: "0" }, 30);
+    // }
+
+    // On scroll show the nav bar
+    let lastScrolTop = 0;
+    $(window).on('scroll', function () {
+        // console.log($(window).scrollTop());
+        let scrollTop = $(this).scrollTop();
+        let navbarElement = $("nav.navbar");
+        if (scrollTop >= 100 && scrollTop < 470) {
+            navbarElement.addClass("navbar-scrolled");
+        } else if (scrollTop >= 470) {
+            if (lastScrolTop > scrollTop) {
+                navbarElement.css({ top: '0px' })
+            } else {
+                navbarElement.css({ top: '-80px' })
+            }
+        } else {
+            navbarElement.removeClass("navbar-scrolled");
+        }
+
+        lastScrolTop = scrollTop;
+    });
+
+
     // Landing page height
 
-    var windowHeight = $(window).height(),
-        navHeight = $('.navbar').innerHeight();
+    var windowHeight = $(window).height();
 
     // $('.landing-page').height();
-    $('.landing-page').css('min-height', windowHeight - navHeight);
+    $('.landing-page').css('min-height', windowHeight);
 
 
 
@@ -36,5 +70,13 @@ $(document).ready(function () {
         $(this).children('.works-description').hide(200);
     });
 
-    // $('.carousel').carousel()
+
+
+
+    // Smooth scroling
+    $('.nav-link').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
 });
